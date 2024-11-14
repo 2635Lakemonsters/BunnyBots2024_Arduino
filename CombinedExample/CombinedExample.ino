dbcd15fe946fa30d399683ae6facfbc0dc9c13af
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2024-11-13 20:41:23 -0800

    Improved detection process and starter calibration values
    We changed the integration time of the color sensor to be 2.4 milliseconds rather than 614. And changed the threshold value system. Progressed on code cleaning.

--- CombinedExample/CombinedExample.ino
+++ CombinedExample/CombinedExample.ino
@@ -5,9 +5,12 @@
 #define TCAADDR       0x70
 #define LEFTSENSOR    2
 #define RIGHTSENSOR   3
-#define STRIP_PIN     13
+#define STRIP_PIN     4
 #define LED_COUNT     55
 #define RED_RIGHT_PIN 8
+#define BLUE_RIGHT_PIN 9
+#define RED_LEFT_PIN 10
+#define BLUE_LEFT_PIN 11
 
 
 struct Color_Type {
@@ -19,16 +22,15 @@ struct Color_Type {
 };
 
 // The threshold for the ratio between red and blue that signifies whether a red balloon or blue balloon are seen
-const float redTolerance = 1.80;
-const float blueTolerance = 0.9;
-
+const float midThresh = 2;
+const float midThreshWidth = 1.3;
 // This value is used to tell which sensor is currently being measured
 bool rightSensor = false;
 
 // The LED ranges that define the separate parts of the LED strip
-int leftSideLEDs[2] = {0,19};
-int middleLEDs[2] = {20,34};
-int rightSideLEDs[2] = {35,54};
+int leftSideLEDs[2] = {0,1}; // {0,19}
+int middleLEDs[2] = {2,3};// {20,34}
+int rightSideLEDs[2] = {4,5};// {35,54}
 
 // Strip Setup Constants
 Adafruit_NeoPixel strip(LED_COUNT, STRIP_PIN, NEO_GRB + NEO_KHZ800);
@@ -44,6 +46,7 @@ uint16_t g;
 uint16_t b;
 uint16_t c;
 
+ // Sections of the LED strip
 enum Sides {
   NONE,
   LEFT,
@@ -51,6 +54,7 @@ enum Sides {
   RIGHT,
 };
 
+// switches the selected address of the multiplexer
 void tcaSelect(uint8_t i) {
   if (i > 7) return;
  
@@ -59,112 +63,112 @@ void tcaSelect(uint8_t i) {
   Wire.endTransmission();  
 }
 
-// initialize sensors
-
-
+// Defining helper functions
 void setColors(Sides, uint32_t);
 int checkColor(uint32_t, uint32_t, uint32_t);
 
+// Initializing sensors
+Adafruit_TCS34725 lightSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_1X);
 
-Adafruit_TCS34725 lightSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 void setup() {
-  // put your setup code here, to run once:
+  //Initializing data pins
   pinMode(RED_RIGHT_PIN, OUTPUT);
 
-  
+  // Starting everything up (comms and sensors)
+  Serial.begin(115200);
   strip.begin();
   strip.setBrightness(20);
-  //strip.setPixelColor(5, 50, 50, 255); // (pixel index, red, green, blue)
   strip.show();
-
-  Serial.begin(115200);
   lightSensor.begin();
   tcaSelect(LEFTSENSOR);
   delay(100);
-  
 }
 
-  void loop(){
-  tcaSelect(LEFTSENSOR);
-  rightSensor = false;
-  lightSensor.getRawData(&r,  &g,  &b,  &c);
-  Serial.println("LEFT SENSOR");
-  Serial.print("RED: ");
-  Serial.println(r, DEC);
-  Serial.print("GREEN: ");
-  Serial.println(g, DEC);
-  Serial.print("BLUE: ");
-  Serial.println(b, DEC);
-  Serial.println();
-  delay(100);
-  setSideColors();
+void loop(){
+  //tcaSelect(LEFTSENSOR);
+  //rightSensor = false;
+  //lightSensor.getRawData(&r,  &g,  &b,  &c);
+  //Serial.println("LEFT SENSOR");
+  // Serial.print("RED: ");
+  // Serial.println(r, DEC);
+  // Serial.print("GREEN: ");
+  // Serial.println(g, DEC);
+  // Serial.print("BLUE: ");
+  // Serial.println(b, DEC);
+  // Serial.println();
+  //delay(100);
+  //setSideColors();
   tcaSelect(RIGHTSENSOR);
   rightSensor = true;
   lightSensor.getRawData(&r,  &g,  &b,  &c);
   Serial.println("RIGHT SENSOR");
-  Serial.print("RED: ");
-  Serial.println(r, DEC);
-  Serial.print("GREEN: ");
-  Serial.println(g, DEC);
-  Serial.print("BLUE: ");
-  Serial.println(b, DEC);
-  Serial.println();
+  // Serial.print("RED: ");
+  // Serial.println(r, DEC);
+  // Serial.print("GREEN: ");
+  // Serial.println(g, DEC);
+  // Serial.print("BLUE: ");
+  // Serial.println(b, DEC);
+  // Serial.println();
+  delay(100);
   setSideColors();
-
-
 }
 
 void setSideColors(){
-  if (checkColor(r,g,b) == Color_Type::RED && rightSensor == false){
-    Serial.println("RED");
+  int color = checkColor(r,g,b);
+
+
+  
+
+
+  if (color == Color_Type::RED && rightSensor == false){
+    // Serial.println("RED");
     setColors(LEFT, red);
   }
 
-  if (checkColor(r,g,b) == Color_Type::RED && rightSensor == true){
-    Serial.println("RED");
+  if (color == Color_Type::RED && rightSensor == true){
+    // Serial.println("RED");
     setColors(RIGHT, red);
     digitalWrite(RED_RIGHT_PIN, HIGH);
   }
 
-  if (checkColor(r,g,b) == Color_Type::BLUE && rightSensor == false){
-    Serial.println("BLUE");
+  if (color == Color_Type::BLUE && rightSensor == false){
+    // Serial.println("BLUE");
     setColors(LEFT, blue);
   }
 
-  if (checkColor(r,g,b) == Color_Type::BLUE && rightSensor == true){
-    Serial.println("BLUE");
+  if (color == Color_Type::BLUE && rightSensor == true){
+    // Serial.println("BLUE");
     setColors(RIGHT, blue);
     digitalWrite(RED_RIGHT_PIN, LOW);
   }
 
-  if (checkColor(r,g,b) == Color_Type::NONE && rightSensor == false){
-    Serial.println("NONE");
+  if (color == Color_Type::NONE && rightSensor == false){
+    // Serial.println("NONE");
     setColors(LEFT, green);
     
   }
 
-  if (checkColor(r,g,b) == Color_Type::NONE && rightSensor == true){
-    Serial.println("NONE");
+  if (color == Color_Type::NONE && rightSensor == true){
+    // Serial.println("NONE");
     setColors(RIGHT, green);
     digitalWrite(RED_RIGHT_PIN, LOW);
   }
 }
+
 int checkColor(uint32_t red, uint32_t green, uint32_t blue){
-  double tempblue = (double) blue;
-  double tempred = (double) red;
-  Serial.println(tempred);
-  Serial.println(tempblue);
-  Serial.println(tempred / tempblue);
-  if (tempred / tempblue > redTolerance)
-  {
+  float tempblue = (float) blue;
+  float tempred = (float) red;
+  float ratio = tempred/tempblue;
+  // Serial.println(tempred);
+  // Serial.println(tempblue);
+  Serial.print("Ratio:");
+  Serial.println(ratio);
+
+  if (ratio > midThresh + midThreshWidth) {
     return Color_Type::RED;
-  }
-  else if (tempblue/ tempred > blueTolerance) 
-  {
+  } else if (ratio <  midThresh - midThreshWidth) {
     return Color_Type::BLUE;
-  }
-  else
-  {
+  } else {
     return Color_Type::NONE;
   }
 }
@@ -201,5 +205,3 @@ void setColors(Sides side, uint32_t color){
   }
   strip.show();
 }
-
-// const double btmp{static_cast<double>(blue)};
