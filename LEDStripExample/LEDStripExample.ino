31ab665935dc44f5ea513530b6151025646e81fc
VenomAI <jht8287@gmail.com>
2024-12-13 02:58:17 -0800

    Added second strip to example

--- LEDStripExample/LEDStripExample.ino
+++ LEDStripExample/LEDStripExample.ino
@@ -1,19 +1,17 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN    5
-#define LED_COUNT  35
+#define LEFT_STRIP_PIN    5
+#define RIGHT_STRIP_PIN   6
+#define LED_COUNT  21
 
-int leftSideLEDs[2] = {0,4};
-int middleLEDs[2] = {5,9};
-int rightSideLEDs[2] = {10,14};
+Adafruit_NeoPixel strip_left(LED_COUNT, LEFT_STRIP_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_right(LED_COUNT, RIGHT_STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-uint32_t magenta = strip.Color(255, 0, 255); //RGB
-uint32_t green = strip.Color(0, 255, 0);
-uint32_t red = strip.Color(255, 0, 0);
-uint32_t blue = strip.Color(0, 0, 255);
-uint32_t no_colour = strip.Color(0, 0, 0);
+uint32_t magenta = strip_left.Color(255, 0, 255); //RGB
+uint32_t green = strip_left.Color(0, 255, 0);
+uint32_t red = strip_left.Color(255, 0, 0);
+uint32_t blue = strip_left.Color(0, 0, 255);
+uint32_t no_colour = strip_left.Color(0, 0, 0);
 
 enum Sides {
   NONE,
@@ -22,16 +20,20 @@ enum Sides {
   RIGHT,
 };
 
-//void setColors(Sides, uint32_t);
+void setColors(Sides, uint32_t);
 
 void setup() {
   // put your setup code here, to run once:
   
-  strip.begin();
-  strip.setBrightness(5);
-  //strip.setPixelColor(5, 50, 50, 255); // (pixel index, red, green, blue)
-  strip.show();
-  
+  strip_left.begin();
+  strip_left.setBrightness(20);
+  // strip_left.setPixelColor(0, 50, 50, 255); // (pixel index, red, green, blue)
+  strip_left.show();
+
+  strip_right.begin();
+  strip_right.setBrightness(20);
+  // strip_right.setPixelColor(0, 50, 50, 255); // (pixel index, red, green, blue)
+  strip_right.show();
 }
   void loop(){
 
@@ -42,62 +44,40 @@ void setup() {
     // having magenta in the middle represents having ballons of the wrong colors grabbed or no ballons grabbed on either side or both sides
     // having green on all side represetns having the correct color ob ballons grabbed
     
-
     setColors(LEFT, green);
-    setColors(MIDDLE, green);
     setColors(RIGHT, green);
     delay(500);
 
-    setColors(NONE, no_colour);
-    
-    delay(500);
-
     setColors(LEFT, red);
-    setColors(MIDDLE, red);
     setColors(RIGHT, red);
     delay(500);
 
+    setColors(LEFT, no_colour);
+    setColors(RIGHT, no_colour);
+    delay(500);
+
     setColors(LEFT, magenta);
-    setColors(MIDDLE, magenta);
     setColors(RIGHT, magenta);
     delay(500);
 
     setColors(LEFT, blue);
-    setColors(MIDDLE, blue);
     setColors(RIGHT, blue);
     delay(500);
   }
 
 
 void setColors(Sides side, uint32_t color){
-  int start;
-  int end;
-  
-
   switch (side){
     case LEFT:
-      start = leftSideLEDs[0];
-      end = leftSideLEDs[1];
-      break;
-    case MIDDLE:
-      start = middleLEDs[0];
-      end = middleLEDs[1];
+      strip_left.fill(color);
+      strip_left.show();
       break;
     case RIGHT:
-      start = rightSideLEDs[0];
-      end = rightSideLEDs[1];
+      strip_right.fill(color);
+      strip_right.show();
       break;
 
-    default: 
-      start = 0;
-      end = LED_COUNT-1;
-      //strip.setPixelColor(0, blue);
+    default:
       break;
-
   }
-  
-  for (int i = start; i <= end; i++){  
-    strip.setPixelColor(i, color);
-  }
-  strip.show();
 }
